50d104b46882bf6dd68e882abdb8b87709872e69
Nusrat Jahan Nidhi <nusrat.nidhi7801@gmail.com>
2025-03-20 17:32:06 +0600

    Added some files

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js"; // import connectDB function from db.js
-import Product from "./models/product.model.js"; // import Product model
+
+import { productRoutes } from "./routes/product.route.js";// ✅ Import productRoutes
 
 dotenv.config(); // to access .env file
 
@@ -9,42 +10,7 @@ const app = express(); // create express app
 
 app.use(express.json()); // to parse json data
 
-app.post("/api/products", async (req, res) => {
-
-    const product = req.body;
-
-    if (!product.name || !product.price || !product.image) {
-        return res.status(400).json({success : false , message: "All fields are required" });
-    }
-
-
-    const newProduct = new Product(product);
-
-    try{
-        await newProduct.save();
-        res.status(201).json({success: true, message: "Product added successfully" , data: newProduct});
-    }
-    catch (error) {
-        console.error("Error in create product ",error.message);
-        res.status(500).json({success: false, message: "Internal Server Error"});
-    }
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-    const {id} = req.params;
-    // console.log("id :",id);
-
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({success: true, message: "Product deleted successfully"});
-    }
-
-    catch{
-    
-        res.status(404).json({success: false, message: "Product not found in the database!"});
-    }
-});
-
+app.use("/api/products", productRoutes); // ✅{
 
 app.listen(5000, () => {
   connectDB(); // call connectDB function
